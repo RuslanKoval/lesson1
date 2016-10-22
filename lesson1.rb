c2583111014d8520b8ae64dbb4c2ceff6b24118a
Ruslan Koval <ruslan.koval21@gmail.com>
2016-10-22 22:04:46 +0300

    Lesson_1

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,27 @@
 require 'date'
 class Lesson1
   def sum(val = 0)
-    # TODO
+    val.to_s.split('').map(&:to_i).inject { |a, e| a + e }
   end
 
   def age(birthday)
-    # TODO
+    if birthday.nil?
+      'Invalid Date Format'
+    else
+      now = Date.parse(Time.now.to_s)
+      day = (now - Date.parse(birthday)).to_i
+      string = "I live #{day / 365} years or #{day}, days or #{day * 24} hours,
+       or #{day * 24 * 60} minutes, or #{day * 24 * 60 * 60} seconds"
+      string
+    end
   end
 
   def name
-    # TODO
+    puts "What's your name? "
+    name = gets
+    second_name = gets
+    father_name = gets
+    name_out = "Hello #{name} #{second_name} #{father_name}!"
+    name_out
   end
 end
